1a827198fa50a1972642e7d0f68226eefd446ec6
SilvaFernandaAM <silvafernanda.oliveira@gmail.com>
2025-06-13 11:40:25 -0400

    dia_semana

---
+++ operadores_logicos/type.js
@@ -0,0 +1,27 @@
+var dia = "Quarta";
+
+switch (dia) {
+    case "Segunda":
+        console.log("Dia de começar a semana!");
+    break;
+
+    case "Terça":
+        console.log("Hora de aprender coisas novas!");
+    break;
+
+    case "Quarta":
+        console.log("Metade da semana. Continue firme!");
+    break;
+
+    case "Quinta":
+        console.log("Quase lá! Um pouco mais de esforço!");
+    break;
+
+    case "Sexta":
+        console.log("Dia de comemorar, o fim de semana chegou!");
+    break;
+
+    default:
+        console.log("Fim de semana chegou")
+    break;
+}
